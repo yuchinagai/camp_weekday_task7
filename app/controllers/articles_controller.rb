199f3517b47cd08c0d7ffb2f68d60ea6b6341eb8
YuchiNagai <alone.on.the.rope1231@gmail.com>
2019-03-14 15:36:17 +0900

    記述の修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,7 @@
 class ArticlesController < ApplicationController
-  before_action :set_post, only: [:edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def show
-    @article  = Article.find(params[:id])
   end
 
   def index
